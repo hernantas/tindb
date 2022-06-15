f554fb2577fcc1f91d63d0f7e68febb725dd051e
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-06-15 12:07:15 +0700

    feat: add `JSONAdapter`

---
+++ src/adapter/JSONAdapter.ts
@@ -0,0 +1,19 @@
+import { Adapter } from './Adapter'
+import { TextAdapter } from './TextAdapter.js'
+
+export class JSONAdapter implements Adapter<unknown> {
+  private readonly adapter: TextAdapter
+
+  constructor(filepath: string) {
+    this.adapter = new TextAdapter(filepath)
+  }
+
+  async read(): Promise<unknown | undefined> {
+    const data = await this.adapter.read()
+    return data !== undefined && data !== null ? JSON.parse(data) : undefined
+  }
+
+  async write(data: unknown): Promise<void> {
+    await this.adapter.write(JSON.stringify(data))
+  }
+}
