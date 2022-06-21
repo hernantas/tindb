3c60f3c2cea627c424b25efb9ddb92cdc589c2e9
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-06-21 14:00:36 +0700

    refactor: replace writing to file to be buffered

--- src/adapter/TextAdapter.ts
+++ src/adapter/TextAdapter.ts
@@ -1,21 +1,25 @@
 import { readFile, writeFile } from 'fs/promises'
 import { Adapter } from './Adapter.js'
+import { BufferedWriter } from './BufferedWriter.js'
 import { ReadFn } from './ReadFn.js'
 import { WriteFn } from './WriteFn.js'
 
 export class TextAdapter implements Adapter<string> {
+  private readonly writer: BufferedWriter<string>
+
   constructor(
     filepath: string,
     private readonly readFn: ReadFn<string> = () => readFile(filepath, 'utf-8'),
-    private readonly writeFn: WriteFn<string> = (data) =>
-      writeFile(filepath, data, 'utf-8')
-  ) {}
+    writeFn: WriteFn<string> = (data) => writeFile(filepath, data, 'utf-8')
+  ) {
+    this.writer = new BufferedWriter(writeFn)
+  }
 
   async read(): Promise<string> {
     return await this.readFn()
   }
 
   async write(data: string): Promise<void> {
-    await this.writeFn(data)
+    await this.writer.write(data)
   }
 }
